0726008c3ab3998ad4b18847df4e45c4fff23ef1
Naooo <naooolaf@gmail.com>
2026-03-03 23:05:08 +0700

    s

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -21,7 +21,7 @@ export default function Welcome({
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? ( 
-                            <Link
+                            <Link 
                                 href={dashboard()}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                             >
