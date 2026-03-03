92fdaa11eba881d87738d9f1ecd519d17039aa4c
Naooo <naooolaf@gmail.com>
2026-03-03 23:05:34 +0700

    2

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -25,7 +25,7 @@ export default function Welcome({
                                 href={dashboard()}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                             >
-                                Dashboard
+                                Dashboardd
                             </Link>
                         ) : (
                             <>
