96a3240df863e8cc85c961777d10ffe59f3d3548
Naooo <naooolaf@gmail.com>
2026-03-03 23:03:27 +0700

    2

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -20,7 +20,7 @@ export default function Welcome({
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
-                        {auth.user ? (
+                        {auth.user ? ( 
                             <Link
                                 href={dashboard()}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
