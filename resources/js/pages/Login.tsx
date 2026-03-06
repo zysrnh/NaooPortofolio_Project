d5e32f06261c462500967eda973c3546d4d9c51c
Naooo <naooolaf@gmail.com>
2026-03-06 22:04:49 +0700

    a

--- resources/js/pages/Login.tsx
+++ resources/js/pages/Login.tsx
@@ -13,7 +13,7 @@ const IconEye = () => (
     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
     <circle cx="12" cy="12" r="3"/>
   </svg>
-);
+); 
 const IconEyeOff = () => (
   <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#0B1957" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
     <path d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24"/>
