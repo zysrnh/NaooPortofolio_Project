ade36dbd344c9af9fd1339555cdcc6099d31208f
Naooo <naooolaf@gmail.com>
2026-07-28 15:53:12 +0700

    feat: configure application kernel and bootstrap service provider with custom middleware and security defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         $this->configureDefaults();
         $this->configureFortify();
     }
